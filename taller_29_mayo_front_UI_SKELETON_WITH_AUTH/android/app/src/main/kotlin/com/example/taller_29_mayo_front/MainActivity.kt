93d22296eefbfda2aaf1dac06cfadc809d77f883
Carlos Parra <altrya-dev@hotmail.com>
2024-05-22 10:54:32 +0200

    reset repository

--- taller_29_mayo_front_UI_SKELETON_WITH_AUTH/android/app/src/main/kotlin/com/example/taller_29_mayo_front/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.taller_29_mayo_front
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- taller_29_mayo_front_UI_SKELETON_WITH_AUTH/lib/app/utils/toast.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-void showToast(BuildContext context, String message) {
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    content: Text(message),
-  ));
-}
